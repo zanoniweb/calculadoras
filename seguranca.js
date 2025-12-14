985292c61f9c2543bbe62a92de0da2f59f416030
zanoniweb <163794616+zanoniweb@users.noreply.github.com>
2025-12-14 09:51:22 -0300

    Add files via upload

---
+++ seguranca.js
@@ -0,0 +1,31 @@
+// Dados dos usuários para login
+        const users = [
+            { username: "jzanoniweb", password: "senha123adm", page: "geracaodocumentosservidores.html" },
+            { username: "dloni", password: "senha457adm", page: "geracaodocumentosservidores.html" },
+            { username: "danicadastramento", password: "senha897adm", page: "geracaodocumentosservidores.html" },
+            { username: "joaocadastramento", password: "senha675adm", page: "geracaodocumentosservidores.html" },
+            { username: "aimecadastramento", password: "senha436adn", page: "geracaodocumentosservidores.html" },
+            { username: "alemaodiretor", password: "senha236adm", page: "geracaodocumentosservidores.html" },
+            { username: "carlosdivida", password: "senha423adn", page: "geracaodocumentosservidores.html" },
+
+        ];
+
+        function login() {
+            const usernameInput = document.getElementById("username").value;
+            const passwordInput = document.getElementById("password").value;
+            const errorMessage = document.getElementById("error-message");
+
+            // Limpa a mensagem de erro
+            errorMessage.textContent = "";
+
+            // Encontra o usuário correspondente no array
+            const user = users.find(u => u.username === usernameInput && u.password === passwordInput);
+
+            if (user) {
+                // Se o usuário e a senha estiverem corretos, redireciona para a página apropriada
+                window.location.href = user.page;
+            } else {
+                // Se o login falhar, exibe uma mensagem de erro
+                errorMessage.textContent = "Usuário ou senha incorretos!";
+            }
+        }
